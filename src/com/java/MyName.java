59bbb9ba5f9f8253e7c91abb809d61fae7f7df4e
MariaShinyAmala <mariashiny57@gmail.com>
2023-10-25 15:16:13 +0530

    new

--- src/com/java/MyName.java
+++ src/com/java/MyName.java
@@ -3,6 +3,6 @@ package com.java;
 public class MyName {
 public static void main(String[] args) {
 	System.out.println("My Name is Shiny");
-	System.out.println("My Daughter's name is Aswathy");
+	System.out.println("My Daughter's name is Aswathy- one branch");
 }
 }
